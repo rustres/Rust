3bec9c254d72a9b541d0eb9c546d6947f699f164
rustres <93039875+rustres@users.noreply.github.com>
2021-10-25 18:06:41 +0300

    rust: 0.1

--- Rustify/helper_funcsions/sql_helpers/__init__.py
+++ Rustify/helper_funcsions/sql_helpers/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from CyberPro import DB_URI
+from Rustify import DB_URI
 
 
 def start() -> scoped_session:
