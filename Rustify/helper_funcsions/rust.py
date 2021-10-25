93f3a65713e1da2c0706ea81a234f26fe28b8cf2
rustres <93039875+rustres@users.noreply.github.com>
2021-10-25 18:00:22 +0300

    rust: 0.1

--- Rustify/helper_funcsions/rust.py
+++ Rustify/helper_funcsions/rust.py
@@ -3,15 +3,15 @@ import os, sys, platform
 from dotenv import load_dotenv
 from math import ceil
 import time
-from CyberPro import HU_STRING_SESSION, TG_COMPANION_BOT, APP_ID, API_HASH, DB_URI, IS_BOT
+from Rustify import HU_STRING_SESSION, TG_COMPANION_BOT, APP_ID, API_HASH, DB_URI, IS_BOT
 
 
 try:
-    cyber        = Client(
+    rust        = Client(
 HU_STRING_SESSION,
 api_id          = APP_ID,
 api_hash        = API_HASH,
-plugins         = dict(root="CyberPro/plugins")
+plugins         = dict(root="Rustify/plugins")
 )
 except ValueError:
     print("Yazdığınız dəyərlərdən biri yanlışdır!")
